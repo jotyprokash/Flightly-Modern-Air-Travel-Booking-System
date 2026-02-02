9967789e66b742bf7631d2cf4fb8856c138180a1
jotyprokash <jotyprokash61@gmail.com>
2026-02-02 15:11:27 +0600

    Localize route selection to regional destinations

--- frontend/src/components/routeSelector/Routeselector.js
+++ frontend/src/components/routeSelector/Routeselector.js
@@ -2,30 +2,32 @@ import React, { useState } from 'react'
 import './Routeselector.css'
 import * as apiCall from './routeApifunc'
 import BusList from '../BusList/BusList'
+
 export default function Routeselector() {
     const [dataInp, setData] = useState("")
     const [startCity, setStartCity] = useState('')
     const [destination, setDestination] = useState('')
+
     const handleToCity = e => {
         e.preventDefault()
         setDestination({ destination: e.target.value })
         localStorage.setItem("destination", e.target.value)
     }
+
     const renderBusList = (dataInp) => {
         if (Object.keys(dataInp).length > 0) {
             return (<BusList value={dataInp} />)
         }
     }
+
     const handleFromCity = e => {
         e.preventDefault()
         setStartCity({ startCity: e.target.value })
         localStorage.setItem("start", e.target.value)
-        // console.log(startCity)
     }
 
     const getRoutes = e => {
         e.preventDefault()
-        // console.log(startCity,destination)
         apiCall.getRoutesFromApi(startCity.startCity, destination.destination)
             .then(response => response.data)
             .then(data => {
@@ -35,77 +37,68 @@ export default function Routeselector() {
 
     const handleDate = e => {
         e.preventDefault()
-        //    console.log(e.target.value)
         localStorage.setItem("date", e.target.value)
     }
-    
+
     return (
         <div className="rdc">
-            <div className="form-group inline"></div>
             <div className="main-container">
                 <form className="form-inline" onSubmit={e => getRoutes(e)}>
-                    <select name="ad_account_selected" data-style="btn-new" class="selectpicker" onChange={e => { handleFromCity(e) }}>
+
+                    {/* FROM */}
+                    <select
+                        data-style="btn-new"
+                        className="selectpicker"
+                        onChange={e => handleFromCity(e)}
+                    >
                         <option>FROM</option>
-                        <option>Chennai</option>
-                        <option>Bangalore</option>
-                        <option>Coimbatore</option>
-                        <option>Goa</option>
-                        <option>Delhi</option>
-                        <option>Mumbai</option>
-                        <option>Kolkata</option>
-                        <option>Trivandram</option>
-                        <option>Madurai</option>
-                        <option>Cochin</option>
-                        <option>Pune</option>
-                        <option>Hyderabad</option>
-                        <option>Dehradun</option>
-                        <option>Jaipur</option>
-                        <option>Varanasi</option>
-                        <option>Patna</option>
-                        <option>Agra</option>
-                        <option>Kanpur</option>
-                        <option>Lucknow</option>
-                        <option>Indore</option>
-                        <option>Nagpur</option>
-                        <option>Vadodara</option>
-                        <option>Thane</option>
-                        <option>Bhopal</option>
-                        <option>Surat</option>                        
-                        <option>Nashik</option>
-
-
+                        <option>Dhaka</option>
+                        <option>Chattogram</option>
+                        <option>Sylhet</option>
+                        <option>Rajshahi</option>
+                        <option>Khulna</option>
+                        <option>Barishal</option>
+                        <option>Cox&apos;s Bazar</option>
+                        <option>Jessore</option>
+                        <option>Cumilla</option>
+                        <option>Rangpur</option>
+                        <option>Bogura</option>
+                        <option>Saidpur</option>
+                        <option>Pabna</option>
+                        <option>Mymensingh</option>
+                        <option>Dinajpur</option>
+                        <option>Noakhali</option>
+                        <option>Feni</option>
                     </select>
-                    <select name="ad_account_selected" data-style="btn-new" class="selectpicker" onChange={e => { handleToCity(e) }}>
+
+                    {/* TO */}
+                    <select
+                        data-style="btn-new"
+                        className="selectpicker"
+                        onChange={e => handleToCity(e)}
+                    >
                         <option>TO</option>
-                        <option>Hyderabad</option>
-                        <option>Coimbatore</option>
-                        <option>Vishakapatnam</option>
-                        <option>Bangalore</option>
-                        <option>Chenai</option>
-                        <option>Delhi</option>
-                        <option>Mumbai</option>
-                        <option>Kolkata</option>
-                        <option>Trivandram</option>
-                        <option>Madurai</option>
-                        <option>Cochin</option>
-                        <option>Pune</option>                        
-                        <option>Dehradun</option>
-                        <option>Jaipur</option>
-                        <option>Varanasi</option>
-                        <option>Patna</option>
-                        <option>Agra</option>
-                        <option>Kanpur</option>
-                        <option>Lucknow</option>
-                        <option>Indore</option>
-                        <option>Nagpur</option>
-                        <option>Vadodara</option>
-                        <option>Thane</option>
-                        <option>Bhopal</option>
-                        <option>Surat</option>                        
-                        <option>Nashik</option>
+                        <option>Dhaka</option>
+                        <option>Chattogram</option>
+                        <option>Sylhet</option>
+                        <option>Rajshahi</option>
+                        <option>Khulna</option>
+                        <option>Barishal</option>
+                        <option>Cox&apos;s Bazar</option>
+                        <option>Jessore</option>
+                        <option>Cumilla</option>
+                        <option>Rangpur</option>
+                        <option>Bogura</option>
+                        <option>Saidpur</option>
+                        <option>Pabna</option>
+                        <option>Mymensingh</option>
+                        <option>Dinajpur</option>
+                        <option>Noakhali</option>
+                        <option>Feni</option>
                     </select>
-                    <input onChange={e => { handleDate(e) }} type="date"></input>
-                    <input type="submit" className=" btn btn-primary btn-md getRoute" />
+
+                    <input type="date" onChange={e => handleDate(e)} />
+                    <input type="submit" className="btn btn-primary btn-md getRoute" />
                 </form>
 
                 <div>
