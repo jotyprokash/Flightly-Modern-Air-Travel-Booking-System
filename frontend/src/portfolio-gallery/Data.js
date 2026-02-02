82ee8e98102fcbfd9cc704e4e6222d8208e3ec10
jotyprokash <jotyprokash61@gmail.com>
2026-02-02 15:31:06 +0600

    Update flight status listings with regional carriers and routes

--- frontend/src/portfolio-gallery/Data.js
+++ frontend/src/portfolio-gallery/Data.js
@@ -1,118 +1,104 @@
-import React from 'react'
 import one from './images/1.jpg'
-const data =[
-    {
-        id:1,
-        image:{one},
-        title:"Air India ",
-        description:"Chennai to Varnasi - Eta: 20 mins ",
-        category:"Arriving",
-       
-    },
-    {
-        id:2,
-        image:"require('./images/2.jpg')",
-        title:"Indigo",
-        description:"Delhi to Rajasthan - Eta: Delayed by 10 mins ",
-        category:"Delayed",
-       
-    },
-    {
-        id:3,
-        image:"./images/3.jpg",
-        title:"Jet Airways",
-        description:"Goa to Kochi - Eta : Cancelled due to bad Weather",
-        category:"Cancelled",
-       
-    },
-    {
-        id:4,
-        image:"./images/4.jpg",
-        title:"SpiceJet",
-        description:"Coimbatore to Varkala - Eta : 1hr 10 mins",
-        category:"Arriving",
-       
-    },
-    {
-        id:5,
-        image:"./images/5.jpg",
-        title:"British Airways",
-        description:"London to New Delhi - Eta : 2 hr",
-        category:"Arriving",
-       
-    },
-    {
-        id:6,
-        image:"./images/1.jpg",
-        title:"KingFisher",
-        description:"Mumbai to Madurai - Eta :Delayed by 30 mins ",
-        category:"Delayed",
-       
-    },
-    {
-        id:7,
-        image:"./images/2.jpg",
-        title:"KT Airways",
-        description:"Punjab to Guhwati - Eta : Cancelled  ",
-        category:"Cancelled",
-       
-    },
-    {
-        id:8,
-        image:"./images/3.jpg",
-        title:"AirAsia",
-        description:"Sikhhim to Pune - Eta : 52 mins ",
-        category:"Arriving",
-       
-    },
-    {
-        id:9,
-        image:"./images/4.jpg",
-        title:"Vistara",
-        description:"Chennai to Varnasi - Eta: 20 mins ",
-        category:"Arriving",
-       
-    },
-    {
-        id:10,
-        image:"./images/5.jpg",
-        title:"Turkish Airlines",
-        description:"Coimbatore to Varkala - Eta : Delayed by 1 hr ",
-        category:"Delayed",
-       
-    },
-    {
-        id:11,
-        image:"./images/1.jpg",
-        title:"Ethihad",
-        description:"London to New Delhi - Eta : 2 hr ",
-        category:"Arriving",
-       
-    },
-    {
-        id:12,
-        image:"./images/2.jpg",
-        title:"Katy Airways",
-        description:"Manali to Goa - Eta : Cancelled ",
-        category:"Cancelled",
-       
-    },
-    {
-        id:13,
-        image:"./images/3.jpg",
-        title:"Air India",
-        description:"Mumbai to Madurai - Eta :Delayed by 30 mins  ",
-        category:"Delayed",
-       
-    },
-    {
-        id:14,
-        image:"./images/4.jpg",
-        title:"KingFisher",
-        description:"Trichy to Dindugul - Eta : 24 mins",
-        category:"Arriving",
-       
-    },
+
+const data = [
+  {
+    id: 1,
+    image: one,
+    title: "Biman Bangladesh Airlines",
+    description: "Dhaka to Chattogram - ETA: 20 mins",
+    category: "Arriving",
+  },
+  {
+    id: 2,
+    image: "./images/2.jpg",
+    title: "US-Bangla Airlines",
+    description: "Dhaka to Sylhet - Delayed by 10 mins",
+    category: "Delayed",
+  },
+  {
+    id: 3,
+    image: "./images/3.jpg",
+    title: "Novoair",
+    description: "Dhaka to Cox’s Bazar - Cancelled due to weather",
+    category: "Cancelled",
+  },
+  {
+    id: 4,
+    image: "./images/4.jpg",
+    title: "Air Astra",
+    description: "Dhaka to Barishal - ETA: 1 hr 10 mins",
+    category: "Arriving",
+  },
+  {
+    id: 5,
+    image: "./images/5.jpg",
+    title: "Biman Bangladesh (International)",
+    description: "Dhaka to London - ETA: 2 hr",
+    category: "Arriving",
+  },
+  {
+    id: 6,
+    image: "./images/1.jpg",
+    title: "US-Bangla Airlines",
+    description: "Dhaka to Rajshahi - Delayed by 30 mins",
+    category: "Delayed",
+  },
+  {
+    id: 7,
+    image: "./images/2.jpg",
+    title: "Novoair",
+    description: "Dhaka to Jessore - Cancelled",
+    category: "Cancelled",
+  },
+  {
+    id: 8,
+    image: "./images/3.jpg",
+    title: "Air Astra",
+    description: "Dhaka to Saidpur - ETA: 52 mins",
+    category: "Arriving",
+  },
+  {
+    id: 9,
+    image: "./images/4.jpg",
+    title: "Biman Bangladesh Airlines",
+    description: "Chattogram to Dhaka - ETA: 25 mins",
+    category: "Arriving",
+  },
+  {
+    id: 10,
+    image: "./images/5.jpg",
+    title: "Flydubai",
+    description: "Dhaka to Dubai - Delayed by 1 hr",
+    category: "Delayed",
+  },
+  {
+    id: 11,
+    image: "./images/1.jpg",
+    title: "Qatar Airways",
+    description: "Dhaka to Doha - ETA: 2 hr",
+    category: "Arriving",
+  },
+  {
+    id: 12,
+    image: "./images/2.jpg",
+    title: "Biman Bangladesh Airlines",
+    description: "Dhaka to Cox’s Bazar - Cancelled",
+    category: "Cancelled",
+  },
+  {
+    id: 13,
+    image: "./images/3.jpg",
+    title: "US-Bangla Airlines",
+    description: "Sylhet to Dhaka - Delayed by 30 mins",
+    category: "Delayed",
+  },
+  {
+    id: 14,
+    image: "./images/4.jpg",
+    title: "Novoair",
+    description: "Rajshahi to Dhaka - ETA: 24 mins",
+    category: "Arriving",
+  },
 ]
 
-export default data;
+export default data
