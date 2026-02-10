2349c5636d2cba93dd0caf8a85bf3bfa2825c286
Joty <joytiprokash.mondal@g.bracu.ac.bd>
2026-02-10 16:10:19 +0600

    frontend: add static images and visual assets

---
+++ frontend/src/portfolio-gallery/Data.js
@@ -0,0 +1,104 @@
+import one from './images/1.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: one,
+    title: "Biman Bangladesh Airlines",
+    description: "Dhaka to Chattogram - ETA: 20 mins",
+    category: "Arriving",
+  },
+  {
+    id: 2,
+    image: "./images/2.jpg",
+    title: "US-Bangla Airlines",
+    description: "Dhaka to Sylhet - Delayed by 10 mins",
+    category: "Delayed",
+  },
+  {
+    id: 3,
+    image: "./images/3.jpg",
+    title: "Novoair",
+    description: "Dhaka to Cox’s Bazar - Cancelled due to weather",
+    category: "Cancelled",
+  },
+  {
+    id: 4,
+    image: "./images/4.jpg",
+    title: "Air Astra",
+    description: "Dhaka to Barishal - ETA: 1 hr 10 mins",
+    category: "Arriving",
+  },
+  {
+    id: 5,
+    image: "./images/5.jpg",
+    title: "Biman Bangladesh (International)",
+    description: "Dhaka to London - ETA: 2 hr",
+    category: "Arriving",
+  },
+  {
+    id: 6,
+    image: "./images/1.jpg",
+    title: "US-Bangla Airlines",
+    description: "Dhaka to Rajshahi - Delayed by 30 mins",
+    category: "Delayed",
+  },
+  {
+    id: 7,
+    image: "./images/2.jpg",
+    title: "Novoair",
+    description: "Dhaka to Jessore - Cancelled",
+    category: "Cancelled",
+  },
+  {
+    id: 8,
+    image: "./images/3.jpg",
+    title: "Air Astra",
+    description: "Dhaka to Saidpur - ETA: 52 mins",
+    category: "Arriving",
+  },
+  {
+    id: 9,
+    image: "./images/4.jpg",
+    title: "Biman Bangladesh Airlines",
+    description: "Chattogram to Dhaka - ETA: 25 mins",
+    category: "Arriving",
+  },
+  {
+    id: 10,
+    image: "./images/5.jpg",
+    title: "Flydubai",
+    description: "Dhaka to Dubai - Delayed by 1 hr",
+    category: "Delayed",
+  },
+  {
+    id: 11,
+    image: "./images/1.jpg",
+    title: "Qatar Airways",
+    description: "Dhaka to Doha - ETA: 2 hr",
+    category: "Arriving",
+  },
+  {
+    id: 12,
+    image: "./images/2.jpg",
+    title: "Biman Bangladesh Airlines",
+    description: "Dhaka to Cox’s Bazar - Cancelled",
+    category: "Cancelled",
+  },
+  {
+    id: 13,
+    image: "./images/3.jpg",
+    title: "US-Bangla Airlines",
+    description: "Sylhet to Dhaka - Delayed by 30 mins",
+    category: "Delayed",
+  },
+  {
+    id: 14,
+    image: "./images/4.jpg",
+    title: "Novoair",
+    description: "Rajshahi to Dhaka - ETA: 24 mins",
+    category: "Arriving",
+  },
+]
+
+export default data
