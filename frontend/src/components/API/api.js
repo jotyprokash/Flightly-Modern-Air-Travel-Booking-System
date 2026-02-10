13d18f81e9b775fef26ee24de14cdfe6e270c960
Joty <joytiprokash.mondal@g.bracu.ac.bd>
2026-02-10 18:15:59 +0600

    frontend: fix API base URL and auth headers

--- frontend/src/components/API/api.js
+++ frontend/src/components/API/api.js
@@ -1,4 +1,7 @@
-import axios from 'axios'
-export default axios.create({
-    baseURL: 'http//localhost:8080'
-})
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api",
+});
+
+export default api;
